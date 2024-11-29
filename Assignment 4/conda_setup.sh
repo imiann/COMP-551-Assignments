b75041609c61daea5231b45a941bd526d0387019
Ian McFarland <ian.mcfarland8@gmail.com>
2024-11-29 17:57:57 -0500

    Add Files

--- Assignment 4/conda_setup.sh
+++ Assignment 4/conda_setup.sh
@@ -4,15 +4,9 @@
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH --time=01:00:00
+#SBATCH --time=00:10:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=fall2024-comp551
 
-module load cuda/cuda-12.6
 module load miniconda/miniconda-fall2024
-source ~/.bashrc
-conda activate test-jf
-
-echo "TEST" > test_out.txt
-nvidia-smi >> test_out.txt
-python cifar10.py
+conda init bash
