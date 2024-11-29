996b59a8527408b50933588b8b949e1c7b4289f3
Ian McFarland <ian.mcfarland8@gmail.com>
2024-11-29 18:23:18 -0500

    changed folder name

---
+++ Assignment_4/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --nodes 1
+#SBATCH --gres=gpu:1 # request a GPU
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=01:00:00
+#SBATCH --output=%N-%j.out
+#SBATCH --account=fall2024-comp551
+
+module load cuda/cuda-12.6
+module load miniconda/miniconda-fall2024
+source ~/.bashrc
+conda activate test-jf
+
+echo "TEST" > test_out.txt
+nvidia-smi >> test_out.txt
+python cifar10.py
